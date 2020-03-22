5043b5fa3ed2f8897c36778c918167a980edc2b4
Jakub Wójcik <kubaton1994@o2.pl>
2020-03-22 21:24:56 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,17 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+//             // Registration was successful
+//             console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//         }, function(err) {
+//             // registration failed :(
+//             console.log('ServiceWorker registration failed: ', err);
+//         });
+//     });
+// }
 
 // place your code below
 
@@ -36,4 +36,14 @@ var menuSwitcher = document.querySelector('.navigation__switcher--js');
 menuSwitcher.addEventListener('click', (e) => {
     const navigationList = document.querySelector('.navigation__list');
     navigationList.classList.toggle('navigation__list--visible');
+});
+
+
+
+const focusButton = document.querySelector('.focus--js');
+
+focusButton.value = localStorage.getItem('input');
+
+focusButton.addEventListener('keyup', (e) => {
+    localStorage.setItem('input', e.target.value);
 });
